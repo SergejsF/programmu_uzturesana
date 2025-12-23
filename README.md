6c6dec70b9d392f654ca42701dee7e6fd0af1d81
Sergejs Fjodorovs <fjodorovs.s@gmail.com>
2025-12-23 22:01:30 +0200

    finala versija bash scripta un aizpildits readme

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# programu_uzturesana
+# programmu_uzturešana
+Šis Bash skripts veic vairākas darbības ar lietotāja norādīto direktoriju un lejupielādēto attēlu.
+
+Skripts pārbauda, vai lietotājs ir norādījis direktorijas nosaukumu kā parametru.
+Ja direktorija neeksistē, tā tiek izveidota.
+Skripts lejupielādē attēlu (bash.gif) no GitHub repozitorija norādītajā direktorijā.
+Lejupielādētais attēls tiek samazināts līdz 300x300 pikseļiem.
+Attēls tiek automātiski atvērts un pēc 5 sekundēm aizvērts.
+Skripts piedāvā lietotājam izvēli (Y/N), vai dzēst izveidoto direktoriju un tās saturu.
+Ja lietotājs apstiprina, direktorija tiek dzēsta pēc 5 sekundēm.
+
+Lietošana:
+
+1. Piešķir skriptam izpildes tiesības:
+   chmod +x script.sh
+
+2. Izsauc skriptu, norādot direktorijas nosaukumu:
+   ./script.sh mans_mape
+
+3. Piemērs:
+   ./script.sh downloads
--- script.sh
+++ script.sh
@@ -3,8 +3,8 @@
 DIR_NAME="$1"
 
 if [ -z "$DIR_NAME" ]; then
-    echo "Kļūda: Lūdzu norādi direktorijas nosaukumu!"
-    echo "Piemērs: ./script.sh projekts"
+    echo "Kļūda: lūdzu norādiet direktorijas nosaukumu lejupielādei."
+    echo "Piemērs: ./script.sh downloads"
     exit 1
 fi
 
@@ -15,8 +15,28 @@ else
     echo "Direktorija '$DIR_NAME' izveidota."
 fi
 
-sleep 5
+sleep 2
+echo "Sākta faila lejupielāde..."
+wget -P "$DIR_NAME" https://raw.githubusercontent.com/SergejsF/programmu_uzturesana/main/images/bash.gif
+echo "Fails veiksmīgi lejupielādēts $DIR_NAME direktorijā."
+sleep 2
 
-wget 
+FILE="$DIR_NAME/bash.gif"
+sips -z 300 300 "$FILE"
+open "$FILE"
+sleep 5
+osascript -e 'tell application "Preview" to close windows'
+
+echo "Mape $DIR_NAME un tās saturs tiks dzēsts."
+read -p "Vai tiešām vēlaties dzēst? (Y/N): " ANSWER
+
+if [[ "$ANSWER" == "Y" || "$ANSWER" == "y" ]]; then
+    echo "Mape tiks dzēsta pēc 5 sekundēm..."
+    sleep 5
+    rm -r "$DIR_NAME"
+    echo "Mape '$DIR_NAME' ir dzēsta."
+else
+    echo "Dzēšana atcelta. Mape netika dzēsta."
+fi
 
 echo "Darbība pabeigta!"
